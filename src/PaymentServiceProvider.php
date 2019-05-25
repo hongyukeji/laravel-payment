7e81a08ff833502c47218bd4a7b81da13e49a4e3
shadow <admin@hongyuvip.com>
2019-05-25 14:46:48 +0800

    Initial commit

---
+++ src/PaymentServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * +----------------------------------------------------------------------
+ * | laravel-payment [ File Description ]
+ * +----------------------------------------------------------------------
+ * | Copyright (c) 2015~2019 http://www.wmt.ltd All rights reserved.
+ * +----------------------------------------------------------------------
+ * | 版权所有：贵州鸿宇叁柒柒科技有限公司
+ * +----------------------------------------------------------------------
+ * | Author: shadow <admin@hongyuvip.com>  QQ: 1527200768
+ * +----------------------------------------------------------------------
+ * | Version: v1.0.0  Date:2019-05-25 Time:14:38
+ * +----------------------------------------------------------------------
+ */
+
+namespace Hongyukeji\LaravelPayment;
+
+use Hongyukeji\LaravelPayment\Payment;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\ServiceProvider;
+
+class PaymentServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        App::bind('payment', function () {
+            return new Payment;
+        });
+    }
+}
